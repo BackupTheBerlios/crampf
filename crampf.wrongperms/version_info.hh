9dbd525273a0d6f14d45c2e0225c9a77561d95a3
logic <logic>
2000-03-18 04:45:37 +0000

    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !!!!!!!!! DER 02er RELEASE !!!!!!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- crampf.wrongperms/version_info.hh
+++ crampf.wrongperms/version_info.hh
@@ -1,11 +1,11 @@
 //
-// $Id: version_info.hh,v 1.1 2000/03/18 04:38:09 logic Exp $
+// $Id: version_info.hh,v 1.2 2000/03/18 04:45:37 logic Exp $
 //
 
 #ifndef version_info
 #define version_info version_info
 
 #define VERSION         "0.02a"
-#define RELEASENAME     "developers snapshot"
+#define RELEASENAME     "alpha testrelease"
 
 #endif
