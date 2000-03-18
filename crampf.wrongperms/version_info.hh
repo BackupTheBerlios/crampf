5f8cccb07fb75244681d9dfed9e990d8f1378681
logic <logic>
2000-03-18 04:55:42 +0000

    Die Hauptaufgabe der naechsten Version wird es sein die Options konifurierbar zu machen (set commando)

--- crampf.wrongperms/version_info.hh
+++ crampf.wrongperms/version_info.hh
@@ -1,11 +1,15 @@
 //
-// $Id: version_info.hh,v 1.2 2000/03/18 04:45:37 logic Exp $
+// $Id: version_info.hh,v 1.3 2000/03/18 04:55:42 logic Exp $
 //
 
 #ifndef version_info
 #define version_info version_info
 
-#define VERSION         "0.02a"
+#define VERSION         "0.03a"
+#if 0
 #define RELEASENAME     "alpha testrelease"
+#else
+#define RELEASENAME     "developers snapshot"
+#endif
 
 #endif
