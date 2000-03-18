3fde5cdfe1040832c08e45cfc270b4962df648b0
logic <logic>
2000-03-18 12:21:16 +0000

    kline Makefile spielereien

--- crampf/make_tarball.sh
+++ crampf/make_tarball.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This script creates a tar ball for easier distribution
 
-VERSION=`sed -e '/VERSION/{s/[^\"]*\"//;s/".*//;p;};d;' version_info.hh`
+VERSION=`sed -e '/^#define VERSION/{s/[^\"]*\"//;s/".*//;p;};d;' version_info.hh`
 PROGNAME=`basename $(pwd)`
 DISTNAME=$PROGNAME-$VERSION
 ln -s . $DISTNAME
 find $DISTNAME/ -name '*.hh' -or -name '*.cc' -or -name 'Makefile' \
         -or -name 'README.*' -or -name '*.sh' \
-        | xargs tar -hczf $DISTNAME.tar.gz 
+        | xargs tar -hvczf $DISTNAME.tar.gz 
 rm -f $DISTNAME
