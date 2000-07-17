de499424efa90dee8e5c81d1e0c306668ca2a5b4
logic <logic>
2000-07-17 00:38:58 +0000

    configure stuff

--- crampf/make_tarball.sh
+++ crampf/make_tarball.sh
@@ -5,7 +5,8 @@ VERSION=`sed -e '/^#define VERSION/{s/[^\"]*\"//;s/".*//;p;};d;' version_info.hh
 PROGNAME=`basename $(pwd)`
 DISTNAME=$PROGNAME-$VERSION
 ln -s . $DISTNAME
-find $DISTNAME/ -name '*.hh' -or -name '*.cc' -or -name 'Makefile' \
-        -or -name 'README*' -or -name '*.sh' \
+find $DISTNAME/ -name '*.hh' -or -name '*.cc' -or -name 'README*'\
+	-or -name '*.sh' -or -name '*.in' -or -name 'configure' \
+	-or -name 'install-sh' \
         | xargs tar -hvczf $DISTNAME.tar.gz 
 rm -f $DISTNAME
