1a3c7c9d8c56eb1d4f48e3a0dcf76d09d3330ee0
logic <logic>
2000-12-09 16:12:31 +0000

    tarball now build from cvs

--- crampf/make_tarball.sh
+++ crampf/make_tarball.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # This script creates a tar ball for easier distribution
 
+startpwd=$PWD
+mkdir /tmp/crampf-build-$$
+cd /tmp/crampf-build-$$
+cvs -d /data/cvsroot co crampf
+cd crampf
+autoconf
+
 VERSION=`sed -e '/^#define VERSION/{s/[^\"]*\"//;s/".*//;p;};d;' version_info.hh`
 PROGNAME=`basename $(pwd)`
 DISTNAME=$PROGNAME-$VERSION
@@ -9,5 +16,6 @@ find $DISTNAME/ -name '*.hh' -or -name '*.cc' -or -name 'README*'\
 	-or -name '*.sh' -or -name '*.in' -or -name 'configure' \
 	-or -name 'install-sh' -or -name 'COPY*' -or -name 'TODO' \
 	-or -name 'INSTALL*' -or -name 'crampfrc-example' \
-        | xargs tar -hvczf $DISTNAME.tar.gz 
-rm -f $DISTNAME
+        | xargs tar -hvczf $startpwd/$DISTNAME.tar.gz 
+cd $startpwd
+rm -rf /tmp/crampf-build-$$
