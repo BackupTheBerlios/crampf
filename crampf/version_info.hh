4ce80c28b542e00e3f9daa8ab89b8713988c476e
logic <logic>
2000-12-09 16:15:58 +0000

    check wether alpha or developers release now done by make_tarball.sh

--- crampf/version_info.hh
+++ crampf/version_info.hh
@@ -1,12 +1,12 @@
 //
-// $Id: version_info.hh,v 1.18 2000/12/09 16:05:16 logic Exp $
+// $Id: version_info.hh,v 1.19 2000/12/09 16:15:58 logic Exp $
 //
 
 #ifndef VERSION_INFO
 
 #define VERSION_INFO	"0.116a"
 
-#ifdef DIST
+#if 0
 #define RELEASENAME     "alpha testrelease"
 #else
 #define RELEASENAME     "developers snapshot"
