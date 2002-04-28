ad394c98addf3492c632982eae8e9542b3ba74da
logic <logic>
2002-04-28 01:32:05 +0000

    time for a new release :)

--- crampf/version_info.hh
+++ crampf/version_info.hh
@@ -1,10 +1,10 @@
 //
-// $Id: version_info.hh,v 1.21 2002/04/28 00:41:41 logic Exp $
+// $Id: version_info.hh,v 1.22 2002/04/28 01:32:05 logic Exp $
 //
 
 #ifndef VERSION_INFO
 
-#define VERSION_INFO	"0.118a"
+#define VERSION_INFO	"0.120a"
 
 #if 0
 #define RELEASENAME     "alpha testrelease"
