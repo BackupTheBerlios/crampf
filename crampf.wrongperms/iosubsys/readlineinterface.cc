5f27a6a4d8a20c2925fc68dfe784b9bf7061be38
logic <logic>
2002-06-10 01:41:49 +0000

    first word on readline is now completed as command

--- crampf.wrongperms/iosubsys/readlineinterface.cc
+++ crampf.wrongperms/iosubsys/readlineinterface.cc
@@ -8,12 +8,44 @@
 #include <stdlib.h>
 extern "C" {
 #include <readline/readline.h>
+#undef CTRL
 #include <readline/history.h>
 }
 #include "config.hh"
 #include "readlineinterface.hh"
 
 extern struct options* opts;
+
+char *crampf_command_generator( char *text, int state ){
+    static map<string,Command*>::const_iterator cmd_it;
+    static map<string,vector<string> >::const_iterator def_it;
+    static int count, len;
+    char *name;
+    if( !state ){
+	count = 0;
+	cmd_it = opts->cmdmap.cmdmap.begin();
+	def_it = opts->cmdmap.defmap.begin();
+	len = strlen( text );
+    }
+    while( def_it != opts->cmdmap.defmap.end() ){
+	if( strlen(text) == 0 || def_it->first.find( text ) == 0 )
+	    return strdup((def_it++)->first.c_str());
+	def_it++;
+    }
+    while( cmd_it != opts->cmdmap.cmdmap.end() ){
+	if( strlen(text) == 0 || cmd_it->first.find( text ) == 0 )
+	    return strdup((cmd_it++)->first.c_str());
+	cmd_it++;
+    }
+    return (char*)NULL;
+}
+
+char **crampf_completion( char *text, int start, int end ){
+    char **matches = (char**)NULL;
+    if( start == 0 )
+	matches = completion_matches( text, crampf_command_generator );
+    return matches;
+}
 
 ReadLineInterface::ReadLineInterface()
 {
@@ -25,6 +57,7 @@ void
 ReadLineInterface::initialize_readline()
 {
   rl_readline_name = "crampf";
+  rl_attempted_completion_function = (CPPFunction*)crampf_completion;
 }
 
 ReadLineInterface::~ReadLineInterface()
