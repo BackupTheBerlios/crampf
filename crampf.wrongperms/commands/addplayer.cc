f0d185921b185d873156c5c20b4d03e0735831a4
logic <logic>
2003-01-09 09:30:31 +0000

    show currently available players if no argument to addplayer is given

--- crampf.wrongperms/commands/addplayer.cc
+++ crampf.wrongperms/commands/addplayer.cc
@@ -10,10 +10,29 @@ AddPlayer::AddPlayer()
 void
 AddPlayer::doit( const std::string &s )
 {
-      for( PlayerInterface::iterator it = player->begin();
-	      it != player->end(); it++ )
-	  if( ExecPlayer *p = dynamic_cast<ExecPlayer *>(*it) )
-	      p->addPlayer( s );
+      if( s.empty() ){
+	  output->printf("List of currently available external players:\n");
+	  for( PlayerInterface::const_iterator it = player->begin();
+		  it != player->end(); it++ )
+	      if( const ExecPlayer *p = dynamic_cast<const ExecPlayer*>(*it) )
+		  for( std::list<struct ExecPlayer::PlayCmds>::const_iterator at
+			  = p->getPlayCmds().begin(); at != p->getPlayCmds().end(); at++ ){
+		      output->printf( "\t%12s ", at->cmdname.c_str() );
+		      for( std::list<std::string>::const_iterator ot = at->cmdargs.begin();
+			      ot != at->cmdargs.end(); ot++ )
+			  output->printf( "%s ", ot->c_str() );
+		      for( std::list<std::pair<std::string,double> >::const_iterator ot
+			      = at->supportedFilesRegExpAsStrings.begin();
+			      ot != at->supportedFilesRegExpAsStrings.end(); ot++ )
+			  output->printf( "/%s/ %2.2f ", ot->first.c_str(), ot->second );
+		      output->printf( "\n" );
+		  }
+      } else {
+	  for( PlayerInterface::iterator it = player->begin();
+		  it != player->end(); it++ )
+	      if( ExecPlayer *p = dynamic_cast<ExecPlayer *>(*it) )
+		  p->addPlayer( s );
+      }
 }
 
 void
@@ -24,22 +43,7 @@ AddPlayer::help( const std::string &s ) const
   output->printf("If one of the given regular expressions match a filename thats\n");
   output->printf("to be played the player gets a score of <score> (a value between 0.0 and 1.0).\n");
   output->printf("The player with the highest score is used to play back the given filename.\n");
-  output->printf("List of currently available external players:\n");
-  for( PlayerInterface::const_iterator it = player->begin();
-	  it != player->end(); it++ )
-      if( const ExecPlayer *p = dynamic_cast<const ExecPlayer*>(*it) )
-	  for( std::list<struct ExecPlayer::PlayCmds>::const_iterator at
-		  = p->getPlayCmds().begin(); at != p->getPlayCmds().end(); at++ ){
-	      output->printf( "\t%12s ", at->cmdname.c_str() );
-	      for( std::list<std::string>::const_iterator ot = at->cmdargs.begin();
-		      ot != at->cmdargs.end(); ot++ )
-		  output->printf( "%s ", ot->c_str() );
-	      for( std::list<std::pair<std::string,double> >::const_iterator ot
-		      = at->supportedFilesRegExpAsStrings.begin();
-		      ot != at->supportedFilesRegExpAsStrings.end(); ot++ )
-		  output->printf( "/%s/ %2.2f ", ot->first.c_str(), ot->second );
-	      output->printf( "\n" );
-	  }
+  output->printf("If no argument is given the list of current players is printed\n" );
 }
 
 void
